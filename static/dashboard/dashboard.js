8a843f91fd2a9429c972f69c6a643d0d0da73a3a
Rostyslav Bryzgunov <kottenator@gmail.com>
2014-08-10 16:04:58 +0200

    Developing rest of the pages

--- static/dashboard/dashboard.js
+++ static/dashboard/dashboard.js
@@ -1,9 +1,15 @@
 ;(function() {
-    $('.event').click(function() {
-        window.location = $(this).data('detail-url');
-    }).find('a').click(function(e) {
+    var events = $('.event');
+
+    events.find('a').click(function(e) {
         e.preventDefault();
         e.stopPropagation();
         alert('Show popup with contact details');
     });
+
+    var recent = events.filter('.highlighted').addClass('slow-transition');
+    setTimeout(function() {
+        recent.removeClass('highlighted');
+        setTimeout(function() { recent.removeClass('slow-transition'); }, 1500);
+    }, 3000);
 })();
---
+++ static/phone-actions/phone-actions.js
@@ -0,0 +1,5 @@
+;(function() {
+    setTimeout(function() {
+        $('.phone-actions > ul > li > span > .counter').addClass('collapsed');
+    }, 3000);
+})();
