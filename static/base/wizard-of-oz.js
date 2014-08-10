f80ed94e939eb8458bdf3cec957395761273e508
Rostyslav Bryzgunov <kottenator@gmail.com>
2014-08-10 22:11:23 +0200

    Wizard of Oz complete!

--- static/base/wizard-of-oz.js
+++ static/base/wizard-of-oz.js
@@ -1,5 +1,5 @@
 ;(function() {
-    $('.wizard-of-oz a.see-all').click(function(e) {
+    $('.wizard-of-oz .view-all').click(function(e) {
         e.preventDefault();
         window.localStorage.setItem('MobileCare:callsViewed', true);
         window.localStorage.setItem('MobileCare:SMSViewed', true);
@@ -7,11 +7,42 @@
         window.location.reload();
     });
 
-    $('.wizard-of-oz a.unsee-all').click(function(e) {
+    $('.wizard-of-oz .unview-all').click(function(e) {
         e.preventDefault();
         window.localStorage.removeItem('MobileCare:callsViewed');
         window.localStorage.removeItem('MobileCare:SMSViewed');
         $('.content').text('Reloading...');
         window.location.reload();
+    });
+
+    $('.wizard-of-oz .low-battery').click(function(e) {
+        e.preventDefault();
+        $('.phone-info .battery .value').text('1%')
+        $('.phone-info .battery .time-left').text('(5 minutes left)');
+        $('.phone-info .uptime .value').text('85h 39m');
+        $('.low-battery-message').removeClass('hidden');
+        $('.offline-message').addClass('hidden');
+        resetLink.removeClass('hidden');
+    });
+
+    $('.wizard-of-oz .offline').click(function(e) {
+        e.preventDefault();
+        $('.phone-info .battery .value').text('0%')
+        $('.phone-info .battery .time-left').text('(since 12h ago)');
+        $('.phone-info .uptime .value').text('--');
+        $('.low-battery-message').addClass('hidden');
+        $('.offline-message').removeClass('hidden');
+        resetLink.removeClass('hidden');
+    });
+
+    var resetLink = $('.wizard-of-oz .reset');
+    resetLink.click(function(e) {
+        e.preventDefault();
+        $('.phone-info .battery .value').text('39%')
+        $('.phone-info .battery .time-left').text('(5 hours left)');
+        $('.phone-info .uptime .value').text('85h 39m');
+        $('.low-battery-message').addClass('hidden');
+        $('.offline-message').addClass('hidden');
+        $(this).addClass('hidden');
     });
 })();
